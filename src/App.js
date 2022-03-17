47c78d21603ee557dd498b3d24992256ff036c98
Matthew Seidel <matt@Matthews-MacBook.local>
2022-03-17 08:55:42 -0500

    changed repo name, fixed state bug. waiting on api to be useable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Weather from "./weather.container/weather.container";
 
 function App() {
-  // api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=da68121ee70f44aa5ee01ee020383b54
   return (
     <div className="container">
       <Weather />
--- src/weather.container/weather.container.jsx
+++ src/weather.container/weather.container.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./weather.container.css";
 
 function Weather() {
-  const [data, setData] = useState;
+  const [data, setData] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -13,18 +13,22 @@ function Weather() {
 
   async function getData() {
     await axios(
-      "api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=da68121ee70f44aa5ee01ee020383b54"
+      "api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=5&appid=da68121ee70f44aa5ee01ee020383b54"
     )
       .then((res) => setData(res.data))
       .catch((error) => {
-        console.log("error", error);
+        console.log("error");
         setError(error);
       })
       .finally(() => setLoading(false));
   }
   console.log(data);
-  //     "api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=da68121ee70f44aa5ee01ee020383b54"
 
+  //     "api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=5&appid=da68121ee70f44aa5ee01ee020383b54"
+  // icon url format: http://openweathermap.org/img/wn/10d@2x.png
+
+  //return a map of the data to each object renders a weather box
+  //convert temp to faranheight
   return (
     <div className="day">
       <h2 className="day-title">Tue</h2>
