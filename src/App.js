915b9aa7d140ecf9e3f63c4ec661d5fc185021a8
Matthew Seidel <matt@Matthews-MacBook.local>
2022-03-17 08:32:02 -0500

    initial commit. created static mockup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./weather.container/weather.container";
 
 function App() {
+  // api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=da68121ee70f44aa5ee01ee020383b54
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Weather />
     </div>
   );
 }
---
+++ src/weather.container/weather.container.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./weather.container.css";
+
+function Weather() {
+  const [data, setData] = useState;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    await axios(
+      "api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=da68121ee70f44aa5ee01ee020383b54"
+    )
+      .then((res) => setData(res.data))
+      .catch((error) => {
+        console.log("error", error);
+        setError(error);
+      })
+      .finally(() => setLoading(false));
+  }
+  console.log(data);
+  //     "api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=da68121ee70f44aa5ee01ee020383b54"
+
+  return (
+    <div className="day">
+      <h2 className="day-title">Tue</h2>
+      <img
+        className="image"
+        src="http://openweathermap.org/img/wn/10d@2x.png"
+      />
+      <div className="high-low">
+        <h2 className="high">80&#176;</h2>
+        <h2 className="low">40&#176;</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Weather;
